dda142751404e39dd3b41eeeb172e136332af41f
WangJin <wangjin-252@hotmail.com>
2013-05-18 17:41:12 +0800

    fixed CreateUser migration errors and raked

--- db/migrate/20130518091651_create_users.rb
+++ db/migrate/20130518091651_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username :limit => 20
-      t.string :password :limit => 20
-      t.string :realname :limit => 20
-      t.string :cellphone :limit => 11
-      t.string :email :limit => 50
-      t.string :type :limit => 1
+      t.string :username, :limit => 20
+      t.string :password, :limit => 20
+      t.string :realname, :limit => 20
+      t.string :cellphone, :limit => 11
+      t.string :email, :limit => 50
+      t.string :type, :limit => 1
 
       t.timestamps
     end
