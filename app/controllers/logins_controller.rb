b08d04227472e1835d29779f2d1f67adfc276fc0
WangJin <wangjin-252@hotmail.com>
2013-05-18 21:29:10 +0800

    common commit

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -6,4 +6,17 @@ class LoginsController < ApplicationController
 			format.json { render json: @user}
 		end
   end
+
+	def login
+		session[:user_id] = nil
+		if request.post?
+			user=User.find(params[:username],params[:password])
+			if user
+				session[:user_id] = user_id
+				redirect_to index_path
+			else
+				flash[:notice] = "error username or password"
+			end
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :cellphone, :email, :password, :realname, :type, :username
+	validate :username, :presence => true
 end
