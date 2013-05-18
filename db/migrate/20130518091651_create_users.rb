16b63a99c44bb695450e9bad745e8ec2ef7fde49
WangJin <wangjin-252@hotmail.com>
2013-05-18 17:31:33 +0800

    altered create_users migrate

--- db/migrate/20130518091651_create_users.rb
+++ db/migrate/20130518091651_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password
-      t.string :realname
-      t.string :cellphone
-      t.string :email
-      t.string :type
+      t.string :username :limit => 20
+      t.string :password :limit => 20
+      t.string :realname :limit => 20
+      t.string :cellphone :limit => 11
+      t.string :email :limit => 50
+      t.string :type :limit => 1
 
       t.timestamps
     end
