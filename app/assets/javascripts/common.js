46e16847bc2da4d45ebc0ffd46811c1606eb2a2e
WangJin <wangjin-252@hotmail.com>
2013-05-22 15:32:21 +0800

    make edit show

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,3 +1,22 @@
 function submitForm(form){
 	form.submit();
 }
+
+function edit(id){
+	$("#"+id+"_tr").hide();
+	$("#"+id+"_show").show("slide");
+}
+
+function cancel(id){
+	$("#"+id+"_show").hide();
+	$("#"+id+"_tr").show("slide");
+}
+
+function save(id){
+	$("#"+id+"_show").hide();
+	$("#"+id+"_tr").show("slide");
+	realname = $("#"+id+"realname").val();
+	cellphone = $("#"+id+"cellphone").val();
+	email = $("#"+id+"email").val();
+	
+}
