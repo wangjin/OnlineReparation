c5c27bddd4a3873ad676387645b70db99371835e
WangJin <wangjin-252@hotmail.com>
2013-05-22 11:44:52 +0800

    add assoation with reparation_information and user

--- app/models/reparation_information.rb
+++ app/models/reparation_information.rb
@@ -1,4 +1,5 @@
 class ReparationInformation < ActiveRecord::Base
-  attr_accessible :cellphone, :location, :problem_desc, :problem_id, :realname
+  attr_accessible :cellphone, :location, :problem_desc, :problem_id, :realname, :user_id
   belongs_to :problem
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :cellphone, :email, :password, :realname, :usertype, :username, :location
+  has_many :reparation_information
 	validate :username, :presence => true
 end 
